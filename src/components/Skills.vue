1d6239054548b19d94bae44154c29d6557caa871
Jess <iamseye@gmail.com>
2019-08-23 17:24:19 -0400

    ADD: style

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
-    <h1> {{ name }} </h1>
+    <div class="holder">
+        <ul>
+          <li v-for="(data, index) in skills" :key='index'>{{ data.skills }}</li>
+        </ul>
+        <p>These are the skills that you possess.</p>
+    </div>
 
   </div>
 </template>
@@ -10,7 +15,11 @@ export default {
   name: 'Skills',
   data() {
     return {
-      name: 'Jessie'
+      skills: [
+        {"skills": "Vue.js"},
+        {"skills": "Frontend developer"}
+      ],
+      showAlert: true
     }
   }
 }
@@ -18,18 +27,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .holder {
+      background: #fff;
+    }
+
+    ul {
+      margin: 0;
+      padding: 0;
+      list-style-type: none;
+    }
+    
+    ul li {
+      padding: 20px;
+      font-size: 1.3em;
+      background-color: #E0EDF4;
+      border-left: 5px solid #3EB3F6;
+      margin-bottom: 2px;
+      color: #3E5252;
+    }
+
+    p {
+      text-align:center;
+      padding: 30px 0;
+      color: gray;
+    }
+
+    .container {
+      box-shadow: 0px 0px 40px lightgray;
+    }
 </style>
