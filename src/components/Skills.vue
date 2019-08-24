84e585422e69b304ed246efa55f795d1f906e7e7
Jess <iamseye@gmail.com>
2019-08-24 12:52:00 -0400

    ADD: delete function

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,7 +10,9 @@
         </form>
         <ul>
           <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-            <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+            <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}
+              <i class="del-icon fa fa-minus-circle" v-on:click="remove(index)"></i>
+            </li>
           </transition-group>
         </ul>
         <p>These are the skills that you possess.</p>
@@ -41,6 +43,9 @@ export default {
           console.log('Not valid');
         }
       })
+    },
+    remove(id) {
+      this.skills.splice(id, 1);
     }
   }
 }
@@ -49,49 +54,55 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
-      background: #fff;
-    }
+    background: #fff;
+  }
 
-    ul {
-      margin: 0;
-      padding: 0;
-      list-style-type: none;
-    }
-    
-    ul li {
-      padding: 20px;
-      font-size: 1.3em;
-      background-color: #E0EDF4;
-      border-left: 5px solid #3EB3F6;
-      margin-bottom: 2px;
-      color: #3E5252;
-    }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
 
-    p {
-      text-align:center;
-      padding: 30px 0;
-      color: gray;
-    }
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
 
-    .container {
-      box-shadow: 0px 0px 40px lightgray;
-    }
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 
-    input {
-      width: calc(100% - 40px);
-      border: 0;
-      padding: 20px;
-      font-size: 1.3em;
-      background-color: #323333;
-      color: #687F7F;
-    }
-    .alert {
-    background: #fdf2ce;
-    font-weight: bold;
-    display: inline-block;
-    padding: 5px;
-    margin-top: -20px;
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+  }
+
+  .del-icon {
+    cursor: pointer;
+    float: right;
   }
 </style>
